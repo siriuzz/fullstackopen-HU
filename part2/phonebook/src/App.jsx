03b878a81a54b8495629c88bc782ac0687f7b116
Elian Matos <elianmatosd03+github@gmail.com>
2025-04-19 20:05:37 -0400

    ejercicios 2.6 al 2.10

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+// import Filter from './components/Filter'
+// import PersonForm from './components/PersonForm';
+
+function Filter({ nameFilter, setNameFilter }) {
+  return (
+    <p>filter results with name: <input value={nameFilter} onChange={(e) => setNameFilter(e.target.value)} /></p>
+  )
+}
+
+
+function PersonForm({ newName, setNewName, newNumber, setNewNumber, addPerson }) {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      </div>
+      <div>number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
+      <div>
+        <button type="submit" >add</button>
+      </div>
+    </form>
+  )
+}
+function Persons({ namesToShow }) {
+  return (
+    <>
+      {namesToShow.map((person, i) => {
+        return <Person key={i} person={person} />
+      })}
+    </>
+  )
+}
+
+function Person({ person }) {
+  return (
+    <>
+      <p>{person.name} {person.number}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+      number: '1234566'
+    }
+  ])
+  const [newName, setNewName] = useState('');
+  const [nameFilter, setNameFilter] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    if (persons.reduce((prev, curr) => {
+      return curr.name == newName || curr.number == newNumber
+    }, false)) return alert(`The name ${newName} or number ${newNumber} is already added to the phonebook`);
+    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setNewName('');
+    setNewNumber('');
+  }
+
+  const namesToShow = nameFilter === '' ? persons : persons.filter((person) => person.name.toLowerCase() == nameFilter.toLowerCase())
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter nameFilter={nameFilter} setNameFilter={setNameFilter} />
+      <h2>add new person and number</h2>
+      <PersonForm newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} addPerson={addPerson} />
+      <h2>Numbers</h2>
+      <Persons namesToShow={namesToShow} />
+    </div>
+  )
+}
+
+export default App
